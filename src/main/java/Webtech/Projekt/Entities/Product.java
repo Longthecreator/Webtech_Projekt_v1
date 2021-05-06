bd0dfae1f9b1536878ea32fd4f8b3891c65718e0
Long <long.ha@outlook.de>
2021-05-06 21:24:14 +0200

    TL 0605 2123: Product Klasse und ProductRepository Interface angelegt

---
+++ src/main/java/Webtech/Projekt/Entities/Product.java
@@ -0,0 +1,23 @@
+package Webtech.Projekt.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    public Product(){}
+
+    public Product(String name, double price){
+        this.name =name;
+        this.price = price;
+    }
+}
