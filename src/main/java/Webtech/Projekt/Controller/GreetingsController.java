9a86eeae8e1d8e04abf4f2d55777a2b477f48caf
Long <long.ha@outlook.de>
2021-05-06 22:17:15 +0200

    TL 0605 2216: Zwischenspeicherung

--- src/main/java/Webtech/Projekt/Controller/GreetingsController.java
+++ src/main/java/Webtech/Projekt/Controller/GreetingsController.java
@@ -1,10 +1,14 @@
 package Webtech.Projekt.Controller;
 
+import Webtech.Projekt.Entities.Product;
+import Webtech.Projekt.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.env.Environment;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -12,10 +16,19 @@ public class GreetingsController {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @RequestMapping("/")
     public String index() {
         String testEnvValue = Optional.of(env.getProperty("Test_VALUE")).orElse("Environment variable not found");
         return "Hello bad trader! You just tried an environment variable" +testEnvValue;
+    }
+
+    @GetMapping("/products")
+    public List<Product> allProducts(){
+        return
     }
 }
 
--- src/main/java/Webtech/Projekt/Entities/Product.java
+++ src/main/java/Webtech/Projekt/Entities/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -19,5 +20,8 @@ public class Product {
     public Product(String name, double price){
         this.name =name;
         this.price = price;
+    }
+    public List returnAllProducts(){
+
     }
 }
