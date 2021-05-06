df9dc84b15ea7b5540c7abf5e4d323643ec6642c
Long <long.ha@outlook.de>
2021-05-06 11:07:18 +0200

    TL 0605 1106: Heroku Deployment Zwischenstand

--- src/main/java/Webtech/Projekt/Controller/GreetingsController.java
+++ src/main/java/Webtech/Projekt/Controller/GreetingsController.java
@@ -1,13 +1,37 @@
 package Webtech.Projekt.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @RestController
 public class GreetingsController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "Hello world!";
     }
-    //Hard Reset
 }
+    @Entity
+    public class Product {
+        @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private int priceontime;
+
+        public Waehrung() {}
+        public Waehrung(String name,int priceontime) {
+            this.name= name;
+            this.priceontime=priceontime;
+        }
+    }
+
+//    @Autowired
+//    private WaehrungRepository wearungRepository;
+    //Hard Reset
+
