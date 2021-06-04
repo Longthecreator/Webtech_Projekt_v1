c491afa2c90fbd065d50c1f55c31ee665c9960bd
Long <long.ha@outlook.de>
2021-06-04 13:19:35 +0200

    TL 0406 1319: href test 2

--- src/main/java/Webtech/Projekt/config/Endpoints.java
+++ src/main/java/Webtech/Projekt/config/Endpoints.java
@@ -5,5 +5,5 @@ public class Endpoints {
     public static final String INDEX = "/";
     public static final String LOGIN = "/login";
     public static final String COINS = "/coins";
-    //public static final String TRADE = "/trade";
+    public static final String TRADE = "/trade";
 }
