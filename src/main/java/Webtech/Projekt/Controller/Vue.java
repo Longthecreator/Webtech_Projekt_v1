53ae75a5708359047e89ff0a4703a2af237989b1
Long <long.ha@outlook.de>
2021-06-06 19:50:49 +0200

    TL 0606 1950: Funktionierendes Layout

--- src/main/java/Webtech/Projekt/Controller/Vue.java
+++ src/main/java/Webtech/Projekt/Controller/Vue.java
@@ -1,27 +1,27 @@
-package Webtech.Projekt.Controller;
-
-public class Vue {
-// ich mach das hier mal extra wir koennen es ja wenn alles funktioniert kopieren
-    const app =Vue.createApp({})
-
-            app.componenet('buttton-counter',
-
-    {
-        data() {
-        return {count:0}},
-        template:`
-        <button @click="count++" >
-            Du hast {
-        {
-            count
-        }
-    } mal geklickt.
-        </button >`
-    })
-            app.mount('#components-demo')
-
-// bin nicht sicher wo der aufruf hingehoert
-<div id="components-demo">
-    <button-counter></button-counter>
-    </div>
-}
+//package Webtech.Projekt.Controller;
+//
+//public class Vue {
+//// ich mach das hier mal extra wir koennen es ja wenn alles funktioniert kopieren
+//    const app =Vue.createApp({})
+//
+//            app.componenet('buttton-counter',
+//
+//    {
+//        data() {
+//        return {count:0}},
+//        template:`
+//        <button @click="count++" >
+//            Du hast {
+//        {
+//            count
+//        }
+//    } mal geklickt.
+//        </button >`
+//    })
+//            app.mount('#components-demo')
+//
+//// bin nicht sicher wo der aufruf hingehoert
+//<div id="components-demo">
+//    <button-counter></button-counter>
+//    </div>
+//}
