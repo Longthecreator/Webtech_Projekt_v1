542aa9def0552649a9048f6b5b425d219da71c50
Long <long.ha@outlook.de>
2021-06-07 21:22:20 +0200

    TL 0706 2122: okta test

---
+++ src/main/java/Webtech/Projekt/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package Webtech.Projekt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//import java.net.http.HttpRequest;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //public pages
+                .antMatchers(
+                        HttpMethod.GET,
+                        Endpoints.INDEX,
+                        Endpoints.COINS
+                        ).permitAll()
+                .antMatchers(
+                        "javascript/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .logout().logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Client()
+                .and()
+                .oauth2Login();
+
+    }
+}
